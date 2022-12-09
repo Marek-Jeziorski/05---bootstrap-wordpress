2ae7ea05597d3f21836b76fe4cacdee0b914a63e
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-12-09 10:48:44 +0100

    added components manualy

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -10,6 +10,8 @@ import "../styles/styles.scss";
 // Import only the Bootstrap components we need
 import { Util, Dropdown, Offcanvas, Popover } from "bootstrap";
 
+// import Popover from "bootstrap/js/dist/popover";
+
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
   new Popover(popover);
